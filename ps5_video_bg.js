37de09dc59ff4251b4d59a64528956d02b41d50f
Christian <82107238+christian45410@users.noreply.github.com>
2021-05-28 10:34:42 -0500

    Add files via upload

--- ps5_video_bg.js
+++ ps5_video_bg.js
@@ -1,7 +1,7 @@
 // Adds a video background
-// Source: https://github.com/christian45410/UnraidThemes/raw/e596a3694b0f962908a7f2496ae7de1bad3b29d7/ps5_dynamic.mp4
+// Source: https://christian45410.github.io/UnraidThemes/ps5_dynamic.mp4
 
 let video = `
-<div class="background-wrap"><video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> <source src="https:\/\/github.com\/christian45410\/UnraidThemes\/raw\/e596a3694b0f962908a7f2496ae7de1bad3b29d7\/ps5_dynamic.mp4" type="video\/mp4">Video not supported</video></div>
+<div class="background-wrap"><video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> <source src="https:\/\/christian45410.github.com\/christian45410\/UnraidThemes\/ps5_dynamic.mp4" type="video\/mp4">Video not supported</video></div>
 `
 document.body.innerHTML += video
